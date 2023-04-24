a738aadeba5304d3efe926921ceb36e0454c8ec0
daenamdaher <daenamdaher@gmail.com>
2023-04-24 08:31:30 -0300

    Botão de deletar funcionando

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import IndexView, delete_event
 
 urlpatterns = [
-    path("", index, name = "index"),
+    path("", IndexView.as_view(), name = "index"),
+    path('delete_event/<event_id>', delete_event, name = 'delete-event')
 ]
--- core/views.py
+++ core/views.py
@@ -1,26 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Produto
 from .forms import ProdutoModelForm
 from django.contrib import messages
+from django.views.generic import TemplateView
 
-def index(request):
-    if str(request.method) == 'POST':
+
+class IndexView(TemplateView):
+
+    def get(self, request):
+        form = ProdutoModelForm()
+        produtos = Produto.objects.all()
+        context = {
+            'form': form,
+            'produtos': produtos
+        }
+        return render(request, 'index.html', context)
+
+    def post(self, request):
+
         form = ProdutoModelForm(request.POST)
         if form.is_valid():
             form.save()
 
             messages.success(request, "Produto salvo com sucesso!")
             form = ProdutoModelForm()
+
         else:
             messages.error(request, "Erro ao cadastrar produto.")
-    else:
+
         form = ProdutoModelForm()
+        produtos = Produto.objects.all()
+        context = {
+            'form': form,
+            'produtos': produtos
+        }
 
-    produtos = Produto.objects.all()
+        return render(request, 'index.html', context)
 
-    context = {
-        'form' : form,
-        'produtos' : produtos
-    }
-
-    return render(request, 'index.html', context)
+def delete_event(request, event_id):
+    event = Produto.objects.get(pk=event_id)
+    event.delete()
+    return redirect('index')
